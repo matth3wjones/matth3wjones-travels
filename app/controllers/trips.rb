234447df434b13cd28c2d56fc7d2b4b50a688e85
matth3wjones <matthewjones00@me.com>
2017-03-06 15:33:11 -0800

    add show.erb

--- app/controllers/trips.rb
+++ app/controllers/trips.rb
@@ -9,14 +9,29 @@ get '/trips/new' do
 end
 
 post '/trips' do
-  # if logged_in?
-  @trip = Trip.create(destination: params[:destination], description: params[:description], details: params[:details], arrive: params[:arrive], depart: params[:depart], host_id: session[:id])
-  puts params
-  redirect '/trips'
-# end
+  if logged_in?
+    @trip = Trip.create(destination: params[:destination], description: params[:description], details: params[:details], arrive: params[:arrive], depart: params[:depart], host_id: session[:id])
+    redirect '/trips'
+  end
 end
 
 get '/trips/:id' do
   @trip = Trip.find_by(id: params[:id])
   erb :'trips/show'
+end
+
+get '/trips/:id/edit' do
+  @trip = Trip.find(params[:id])
+  erb :"/trips/edit"
+end
+
+put '/trips/:id' do
+  trip = Trip.find(params[:id])
+  trip.update_attributes(params[:trip])
+  redirect '/trips/#{trip.}'
+end
+
+delete '/trips/:id' do
+  trip = Trip.find(params[:id]).destroy
+  redirect '/trips'
 end
