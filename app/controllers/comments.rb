7507e14564dcaa2465b229027124de4357a2c278
matth3wjones <matthewjones00@me.com>
2017-03-09 13:04:36 -0800

    update profile css

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,4 +1,9 @@
-get '/trips/:id/comments' do
-  @trip = Trip.find_by(id: params[:id])
-  @comment = Comment.create(trip_id: params[:id], guest_id: session[:id], text: params[:text])
+post '/trips/:trip_id/comments' do
+  puts params
+  comment = Comment.create(text: params[:text], trip_id: params[:trip_id], user_id: session[:id])
+  comment.valid?
+  puts comment.errors.full_messages
+  @trip = Trip.find_by(id: params[:trip_id])
+  @comment = Comment.all
+  erb :"trips/show"
 end
